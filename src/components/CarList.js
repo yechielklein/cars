d11ce9d62d86f7a3b829e4d273471235f3fe6380
Yechiel Klein <kleinyechiel@gmail.com>
2023-11-13 00:04:30 -0500

    implemented CarList

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -1,7 +1,32 @@
+import { useDispatch, useSelector } from 'react-redux';
+
+import { removeCar } from '../store';
+
 const CarList = () => {
+	const dispatch = useDispatch();
+
+	const cars = useSelector(state => state.cars.data);
+
+	const handleCarDelete = car => {
+		dispatch(removeCar(car.id));
+	};
+
+	const renderedCars = cars.map(car => {
+		return (
+			<div key={car.id} className="panel">
+				<p>{car.name} - ${car.cost}</p>
+				<button
+					className="button is-danger"
+					onClick={() => handleCarDelete(car)}
+				>Delete</button>
+			</div>
+		);
+	});
+
 	return (
-		<div>
-			car list
+		<div className="car-list">
+			{renderedCars}
+			<hr />
 		</div>
 	);
 };
