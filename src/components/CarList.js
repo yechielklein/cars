3dc9a0bbbabfb5da763a026733fc163c0475d818
Yechiel Klein <kleinyechiel@gmail.com>
2023-11-13 19:03:17 -0500

    implemented bolding to warn users of duplicates

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -5,8 +5,13 @@ import { removeCar } from '../store';
 const CarList = () => {
 	const dispatch = useDispatch();
 
-	const cars = useSelector(({ cars: { data, searchTerm } }) => {
-		return data.filter(car => car.name.toLowerCase().includes(searchTerm.toLowerCase()));
+	const { cars, name } = useSelector(({ form, cars: { data, searchTerm } }) => {
+		const filteredCars = data.filter(car => car.name.toLowerCase().includes(searchTerm.toLowerCase()));
+
+		return {
+			cars: filteredCars,
+			name: form.name
+		};
 	});
 
 	const handleCarDelete = car => {
@@ -14,8 +19,10 @@ const CarList = () => {
 	};
 
 	const renderedCars = cars.map(car => {
+		const bold = name && car.name.toLowerCase().includes(name.toLowerCase());
+
 		return (
-			<div key={car.id} className="panel">
+			<div key={car.id} className={`panel ${bold && 'bold'}`}>
 				<p>{car.name} - ${car.cost}</p>
 				<button
 					className="button is-danger"
