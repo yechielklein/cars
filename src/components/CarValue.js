3c366c9d842098161a27aad63ef2b30c1a8400cc
Yechiel Klein <kleinyechiel@gmail.com>
2023-11-13 19:01:33 -0500

    implemented total CarValue

--- src/components/CarValue.js
+++ src/components/CarValue.js
@@ -1,7 +1,15 @@
+import { useSelector } from 'react-redux';
+
 const CarValue = () => {
+	const totalCost = useSelector(({ cars: { data, searchTerm } }) => {
+		const filteredCars = data.filter(car => car.name.toLowerCase().includes(searchTerm.toLowerCase()));
+
+		return filteredCars.reduce((acc, car) => acc + car.cost, 0);
+	});
+
 	return (
-		<div>
-			car value
+		<div className="car-value">
+			Total Cost: ${totalCost}
 		</div>
 	);
 };
